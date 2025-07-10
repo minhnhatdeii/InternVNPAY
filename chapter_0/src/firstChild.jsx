88f168250ebc214771bf658326fb8d4fddbafa88
minhnhatdeii <125042319+minhnhatdeii@users.noreply.github.com>
2025-07-10 15:13:03 +0700

    chapter_0

--- chapter_0/src/firstChild.jsx
+++ chapter_0/src/firstChild.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useEffect } from "react";
 
-const FirstChild = () => {
-    return <p>Toi la First Child!</p>;
+const FirstChild = ({forwardedRef}) => {
+    useEffect(() => {
+    if (forwardedRef?.current) {
+      forwardedRef.current.style.color = "red"; // ví dụ đổi màu từ trong con
+    }
+  }, [forwardedRef]);
+    return <p ref = {forwardedRef}>Toi la First CHild</p>;
 };
 export default FirstChild;
--- chapter_0/src/parentComponent.jsx
+++ chapter_0/src/parentComponent.jsx
@@ -1,12 +1,20 @@
 import React,{Component} from "react";
 import FirstChild from "./firstChild";
-
+import { createRef } from "react";
 class ParentComponent extends Component {
+    constructor(props) {
+    super(props);
+    this.childRef = createRef(); // Cha khởi tạo ref
+  }
+
+  componentDidMount() {
+    console.log(this.childRef.current.textContent); // Đọc nội dung từ con
+  }
     render() {
         return (
         <h1>
             Toi la Parent Component!
-            <FirstChild /> {}
+            <FirstChild forwardedRef={this.childRef} /> {}
         </h1>
         );
     }
